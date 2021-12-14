7c547d12de61548b429257a581a0156bfd810541
Raquel Rosado <rockrosado@gmail.com>
2021-12-14 16:16:09 -0500

    Created SQL tables, edited api

--- final/sql/insert-test-data-api.sql
+++ final/sql/insert-test-data-api.sql
@@ -1,18 +1,8 @@
 INSERT INTO vehicle (make, model, color, capacity, mpg, licensePlate)
 VALUES ('Ford', 'F150', 'black', '5', '25', '123ABC');
-INSERT INTO account (first_name, last_name, email, password)
-VALUES ('Zelda', 'Ziffle', 'zelda@ziffle.com', 'SuperSecret2');
+INSERT INTO driver (id, "userId", "licenseNumber", "licenseState")
+VALUES (1, 12, 'ABC123', 'WI');
 
-id              serial not null
-                        constraint vehicle_id
-                            primary key,
-    make            varchar not null,
-    model           varchar not null,
-    color           varchar not null,
-    vehicleTypeId   serial  not null,
-                        /* constraint vehicle_type_id
-                            foreign key, */
-    capacity        varchar not null,
-    mpg             varchar not null,
-    licenseState    varchar not null,
-    licensePlate
+
+INSERT INTO state (id, "userId", "licenseNumber", "licenseState")
+VALUES (1, 12, 'ABC123', 'WI');
